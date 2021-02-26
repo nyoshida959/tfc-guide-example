15b50cc91bf3c501234fae5d2c02e778d01aa6f7
nyoshida_run-edge <nyoshida@run-edge.com>
2021-02-26 11:35:08 +0900

    2

--- main.tf
+++ main.tf
@@ -2,5 +2,17 @@ module "describe_regions_for_ec2" {
   source     = "./iam_role"
   name       = "describe-regions-for-ec2"
   identifier = "ec2.amazonaws.com"
-  policy     = data.aws_iam_policy_document.allow_describe_regions.json
+  policy     = data.aws_iam_policy_document.s3_full_access.json
+}
+
+data "aws_iam_policy_document" "s3_full_access" {
+  statement {
+    actions = [
+      "s3:*",
+    ]
+
+    resources = [
+      "arn:aws:s3:::*",
+    ]
+  }
 }
