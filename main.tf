25389c576afb2ba8f41d00e8f3e4e8c59983714c
nyoshida_run-edge <nyoshida@run-edge.com>
2021-02-26 13:40:14 +0900

    5

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ provider "aws" {
   region = var.aws_region
 }
 
-# module "describe_regions_for_ec2" {
-#   source     = "./iam_role"
-#   name       = "describe-regions-for-ec2"
-#   identifier = "ec2.amazonaws.com"
-#   policy     = data.aws_iam_policy_document.s3_full_access.json
-# }
+module "describe_regions_for_ec2" {
+  source     = "./iam_role"
+  name       = "describe-regions-for-ec2"
+  identifier = "ec2.amazonaws.com"
+  policy     = data.aws_iam_policy_document.s3_full_access.json
+}
 
 data "aws_iam_policy_document" "s3_full_access" {
   statement {
